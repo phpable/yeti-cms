d9041f7a8f7dd4c640e2d6c95b558173ce929377
hacpaka <hacpaka@eggbe.com>
2019-01-23 15:56:21 +0300

    refactoring

--- modules/yeti@core/resources/views/constants/edit.blade.php
+++ modules/yeti@core/resources/views/constants/edit.blade.php
@@ -2,21 +2,23 @@
 	=> route('yeti@core:constants.update', $Constant->id)])
 
 @section('form')
+	<div class="col-lg-12">
+		<div class="row">
+			<div class="form-group">
+				<label class="control-label">Name</label>
+				<div class="control-body">
+					<input type="text" data-type="name" data-required="true"
+						class="bg-focus form-control parsley-validated" name="name" value="{{ $Constant->name }}">
+				</div>
+			</div>
 
-	<div class="form-group">
-		<label class="col-lg-2 control-label">Name</label>
-		<div class="col-lg-10">
-			<input type="text" data-type="name" data-required="true"
-				class="bg-focus form-control parsley-validated" name="name" value="{{ $Constant->name }}">
+			<div class="form-group">
+				<label class="control-label">Value</label>
+				<div class="control-body">
+					<input type="text" data-type="value" data-required="true"
+						class="bg-focus form-control parsley-validated" name="value" value="{{ $Constant->value }}">
+				</div>
+			</div>
 		</div>
 	</div>
-
-	<div class="form-group">
-		<label class="col-lg-2 control-label">Value</label>
-		<div class="col-lg-10">
-			<input type="text" data-type="value" data-required="true"
-				class="bg-focus form-control parsley-validated" name="value" value="{{ $Constant->value }}">
-		</div>
-	</div>
-
 @stop
